3d4ae54757057d37393637533727791bc2124a1f
YasminCardoso <yasmin.silva@eic.cefet-rj.br>
2018-11-09 15:26:07 -0200

    notas caindo aleatoriamente

--- GuitarHeroProject/Guitarriro/MyWorld.java
+++ GuitarHeroProject/Guitarriro/MyWorld.java
@@ -13,6 +13,13 @@ public class MyWorld extends greenfoot.World
      * Constructor for objects of class MyWorld.
      * 
      */
+    //10% de chance
+    private int noteSpawnRatio = 6;
+    
+    //limite e quantidade
+    private int noteSpawnCount = 0;
+    private int noteSpawnLimit = 4;
+    
     //private Guitarra guitarra;
     private NotaVerde notaVerde;
     private NotaAmarela notaAmarela;
@@ -23,11 +30,18 @@ public class MyWorld extends greenfoot.World
     private BotaoVermelho botaoVermelho;
     private BotaoAzul botaoAzul;
     
+    //Game State
+    private enum GameState{MENU,PLAYING,GAMEOVER};    
+    private GameState state;
     public MyWorld()
     {    
         // Create a new world with 600x400 cells with a cell size of 1x1 pixels.
         super(600, 400, 1); 
         preparePlaying();
+    }
+    
+    public void act(){
+        actPlaying();
     }
     
         private void preparePlaying()
@@ -38,22 +52,46 @@ public class MyWorld extends greenfoot.World
         // Create the menu
         //guitarra = new Guitarra();
         //addObject(guitarra,getWidth()/2,getHeight()/2);
-        notaVerde = new NotaVerde();
-        notaVermelha = new NotaVermelha();
-        notaAmarela = new NotaAmarela();
-        notaAzul = new NotaAzul();
         botaoVerde = new BotaoVerde();
         botaoAmarelo = new BotaoAmarelo();
         botaoVermelho = new BotaoVermelho();
         botaoAzul = new BotaoAzul();
-        addObject(notaVerde, 200, 10);
-        addObject(notaAmarela, 250, 10);
-        addObject(notaVermelha, 300, 10);
-        addObject(notaAzul, 350, 10);
         addObject(botaoVerde, 200, 380);
         addObject(botaoAmarelo, 250, 380);
         addObject(botaoVermelho, 300, 380);
         addObject(botaoAzul, 350, 380);
         // Set the game state
+        state = GameState.PLAYING;
+    }
+    public void actPlaying(){
+        int aleatorio = Greenfoot.getRandomNumber(4);
+        int aleatorio2 = Greenfoot.getRandomNumber(100);
+        notaVerde = new NotaVerde();
+        notaVermelha = new NotaVermelha();
+        notaAmarela = new NotaAmarela();
+        notaAzul = new NotaAzul();
+        if(state != GameState.GAMEOVER){
+            if (noteSpawnCount == 0){
+                if(aleatorio2 < noteSpawnRatio){
+                    if(aleatorio == 0){
+                        addObject(notaVerde, 200, 10);
+                    }
+                    else if(aleatorio == 1){
+                        addObject(notaAmarela, 250, 10);
+                    }
+                    else if(aleatorio == 2){
+                        addObject(notaVermelha, 300, 10);
+                    }
+                    else if(aleatorio == 3){
+                        addObject(notaAzul, 350, 10);
+                    }
+                }
+            }
+            if (noteSpawnCount++ == noteSpawnLimit)
+            {
+                noteSpawnCount=0;
+            }  
+        }
+
     }
 }
